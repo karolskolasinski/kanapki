1812e99bbd5d2340ddf250514fa9c061f0f25562
Karol Skolasiński <karol.skolasinski@7willows.com>
2025-05-13 13:12:36 +0200

    feat: logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,178 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/auth/auth-config";
-
 export default async function Home() {
-  const session = await getServerSession(authOptions);
+  // await new Promise((r) => setTimeout(r, 2000));
 
   return (
-    <div>
-      <div>
-        <strong>JESZCZE CIEPŁE 🔥</strong>: https://jeszczecieple.pl
-      </div>
+    <>
+      <section className="bg-gray-50">
+        <div>
+          <div>
+            {/*<strong>NIEŹLE ZMROŻONE 🥶</strong>: https://niezlezmrozone.pl*/}
+          </div>
+        </div>
 
-      <div>
-        <strong>NIEŹLE ZMROŻONE 🥶</strong>: https://niezlezmrozone.pl
-      </div>
+        <div className="flex flex-col md:flex-row items-center px-4 md:px-16 py-10 md:py-20 gap-10 relative max-w-7xl mx-auto overflow-hidden">
+          <div className="text-center md:text-left sm:w-1/2">
+            <h1 className="text-4xl md:text-6xl font-extrabold mb-8" data-aos="fade-up">
+              HERO
+            </h1>
+          </div>
+        </div>
 
-      <div>
-        {session ? <p>Witaj, {session.user?.name}</p> : (
-          <p>
-            Nie jesteś zalogowany. <a href="/login">Zaloguj się</a>
-          </p>
-        )}
-      </div>
-    </div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aperiam consequuntur
+        culpa debitis delectus doloremque ducimus impedit inventore labore maiores neque nisi
+        officia quaerat ratione similique sunt totam, unde voluptas! Adipisci aliquam deserunt
+        dolore earum, esse, optio quaerat quasi recusandae repellat totam veritatis, voluptatem.
+        Commodi nisi numquam quam quia vel? Adipisci aliquam assumenda culpa dolores illo illum
+        impedit, minima modi necessitatibus nostrum odit qui reiciendis sit, unde veritatis. Alias
+        animi aspernatur consectetur, debitis delectus, dolor doloremque est harum itaque minus
+        nostrum numquam odio quaerat soluta tempora ullam, unde? Consequuntur, corporis cupiditate
+        distinctio ea eius explicabo facilis fugit illum ipsa, modi natus nisi nostrum pariatur quae
+        quidem quo quod quos ratione reprehenderit sapiente sed sint tempora voluptas voluptate
+        voluptatum. Ad magnam modi quisquam rem. Ab ad adipisci assumenda corporis excepturi facilis
+        inventore sit tenetur. Doloremque dolorum ea esse impedit ipsum, iste. Atque autem,
+        doloremque error esse expedita facilis fuga fugit illo laudantium minus natus nihil non
+        numquam officia, pariatur placeat porro quasi quos sequi similique sit ut veritatis!
+        Consequuntur delectus eveniet maxime quasi totam. Accusamus asperiores atque beatae
+        consequuntur dicta dignissimos dolorem error est et exercitationem fuga, illo itaque iure
+        molestiae natus nisi nostrum, officiis placeat possimus ratione reprehenderit saepe sit
+        tempora ut velit vero voluptatibus! Adipisci aperiam aspernatur consequuntur cumque
+        doloribus, ducimus ea eius eos eveniet ex facere facilis harum id illo illum incidunt
+        inventore ipsam laborum libero magnam modi, molestiae mollitia natus nulla officia officiis
+        optio possimus quas quia reiciendis sed similique sunt vero! A accusamus asperiores at
+        beatae commodi cum debitis dicta distinctio earum eius, et eveniet facere fugit incidunt
+        ipsam iste minima molestiae, nam nisi nulla officia pariatur possimus quaerat quidem quis,
+        recusandae reiciendis reprehenderit repudiandae similique soluta suscipit tenetur totam
+        veritatis! Architecto beatae commodi debitis deserunt doloremque ea eligendi eos ex
+        exercitationem fuga inventore ipsum iste itaque iusto, laudantium nam officiis placeat quam
+        quod reiciendis suscipit tempore totam unde veniam voluptatibus. Atque beatae consequuntur
+        debitis doloremque dolorum ea eaque eveniet excepturi id in nam optio pariatur placeat, quas
+        sit tempore unde. A ab animi aut autem blanditiis deleniti deserunt dolorem, doloremque
+        ducimus ex exercitationem facilis fugiat illo impedit iste itaque laudantium modi molestias
+        mollitia nihil non perferendis perspiciatis placeat provident quam, quasi quidem quisquam
+        repellat repellendus sed temporibus vero voluptates voluptatibus? Aspernatur beatae cum
+        dolor esse est excepturi fugit, harum maxime minus nemo omnis quisquam reiciendis unde,
+        voluptate voluptatem. Commodi cum dolorum earum eius esse, odio velit? Consequatur culpa
+        deleniti ea eveniet expedita magnam nihil, numquam perferendis soluta temporibus! Ab ad
+        alias aliquam assumenda at atque beatae consequuntur debitis deleniti deserunt dolor earum
+        enim est, expedita facilis, impedit ipsa labore minus modi molestiae natus nesciunt nihil
+        nobis perferendis porro quibusdam quod ratione repellat repellendus saepe sint tempora,
+        velit voluptate? Fugiat ipsam iusto optio perferendis quae quis ratione rerum saepe, unde?
+        Accusamus corporis ducimus enim error et, eveniet ex fugiat hic optio, placeat porro sequi,
+        ut voluptas. Aspernatur assumenda atque beatae consectetur dicta ducimus eligendi eos ex
+        exercitationem expedita, explicabo facilis in laboriosam laborum laudantium minima nulla
+        numquam perspiciatis quas quia ratione reiciendis sequi suscipit veniam voluptate voluptates
+        voluptatum? Distinctio minus, voluptatem. Consectetur excepturi ipsa iure nam odit sunt.
+        Aliquid expedita explicabo ipsam libero quibusdam, saepe ullam voluptatibus? Accusamus
+        animi, aperiam aspernatur commodi consequuntur culpa cupiditate dolore, doloribus enim et
+        exercitationem ipsum iure iusto necessitatibus neque, nisi nostrum praesentium provident
+        quae soluta ullam unde voluptas! Ab ad aliquam assumenda atque consequuntur cum dignissimos
+        dolore expedita fuga illo illum inventore iste itaque laborum nam, numquam odit optio
+        perferendis perspiciatis, provident quae quam quis quos sit sunt totam ullam vero.
+        Asperiores atque doloribus dolorum ipsa ipsum odio perspiciatis, praesentium quaerat sint
+        veritatis? Deserunt eveniet expedita, explicabo impedit minima nemo nesciunt veritatis.
+        Consequuntur culpa debitis enim et, excepturi fuga hic inventore libero maxime minus modi
+        mollitia nam, neque nostrum omnis porro quas qui quibusdam quisquam tempore totam ut
+        voluptatem. Aliquam architecto consectetur illo inventore possimus reprehenderit soluta
+        sunt, veniam. Accusamus dolore dolorum ea eos eveniet fugit hic laudantium magnam nam natus
+        nobis obcaecati praesentium, provident quibusdam repudiandae saepe, tempore unde veritatis?
+        Aliquid amet aperiam distinctio doloremque doloribus ea eaque ex facilis illum ipsum magni
+        natus pariatur praesentium quaerat sit, veritatis vero voluptas, voluptatem. Accusamus
+        adipisci aliquam aliquid aperiam architecto beatae consectetur dicta dolor doloremque
+        dolorum eius fugit impedit inventore laborum officiis pariatur perferendis quaerat,
+        quibusdam quidem rem repellendus reprehenderit saepe, velit veritatis voluptas. Aliquid,
+        aperiam autem commodi consequatur deleniti earum eos explicabo hic magnam, minima molestiae
+        nisi nobis sequi unde veniam vitae voluptatem. Ab accusamus adipisci aliquam architecto
+        deserunt dignissimos dolorum est eum excepturi, incidunt, inventore iste, minima nemo
+        officia placeat porro quo temporibus. Aliquam ea ex, impedit necessitatibus omnis optio quae
+        quod ratione rerum sed, suscipit ullam, voluptatem. Alias, aut eum illo incidunt ipsam modi
+        perspiciatis repellat rerum, totam unde vel vero, vitae voluptate. Ad architecto
+        consequuntur doloremque dolorum facilis in, laborum laudantium magni minus odio perspiciatis
+        qui quidem repudiandae sunt vel veniam veritatis! Aspernatur cupiditate delectus earum eos
+        hic illum, ipsum molestiae sapiente? A aperiam, assumenda consectetur culpa dicta doloribus
+        esse facilis impedit iste laborum magni molestiae nam obcaecati perferendis quas quia quo,
+        rem sed sit suscipit tenetur unde voluptates! Aliquam delectus eaque in ipsam libero sunt
+        voluptas. Aliquid aperiam blanditiis, cum, cupiditate dolore ea eos eveniet hic in magnam
+        maxime modi molestias nihil optio perspiciatis possimus quas quos rem reprehenderit, saepe
+        sint sunt tenetur totam ullam velit vitae voluptates. Autem, consequuntur cum doloremque est
+        illo ipsam praesentium quos rem totam ut. Accusamus ad, animi aspernatur autem corporis
+        dicta expedita fugit illo ipsa ipsam laborum magni molestiae molestias natus porro possimus
+        quaerat qui rem repellendus reprehenderit similique temporibus voluptatum? Atque cupiditate
+        deleniti, dicta dolor dolore doloribus dolorum, inventore, iste laudantium maiores minima
+        nostrum placeat qui quia quis repellat ullam! Corporis doloremque dolores ducimus enim
+        exercitationem, ipsa iusto maiores temporibus ullam vel? Ad animi autem corporis delectus
+        dolorum earum esse eum facilis hic ipsa itaque iure laborum laudantium magnam magni maxime
+        minima molestiae neque odit pariatur qui quos repellat saepe sapiente, sed sit tempore
+        tenetur ullam voluptatem voluptatum. Amet atque aut culpa doloremque molestiae natus
+        pariatur, qui quod, sint soluta vero voluptatibus! Alias aliquam beatae commodi, delectus
+        excepturi harum minima molestias natus, provident quidem sint, voluptate! Alias aut debitis
+        deserunt eligendi esse est magni odio quae quam sunt. Aliquam aspernatur aut consequatur
+        corporis debitis delectus, itaque libero magni possimus quas quasi saepe tempora ullam.
+        Aperiam asperiores atque enim, eos harum rem voluptatibus! At blanditiis consequuntur
+        debitis dolorum earum est modi omnis, quaerat temporibus veritatis. Consectetur debitis eos
+        fugit incidunt nam porro possimus quasi, veritatis. Ab dicta dolorum eum fugit nam provident
+        quia suscipit tenetur. Commodi dolore dolores dolorum ducimus, labore molestiae mollitia,
+        nostrum provident quos temporibus totam veritatis voluptatum.
+      </section>
+
+      <footer className="bg-gray-100 w-full">
+        <div className="max-w-7xl mx-auto px-4 md:px-16 py-10 md:pt-20 md:pb-0">
+          <div className="flex flex-col lg:flex-row gap-16 pb-8">
+            <div className="flex-1">
+              <h1 className="font-bold uppercase pt-8">Jeszcze ciepłe</h1>
+            </div>
+
+            <div id="contact" className="flex flex-col gap-4 flex-1">
+              <h2 className="text-2xl font-bold">Kontakt</h2>
+
+              <div className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 -960 960 960"
+                  className="min-w-6 w-6"
+                >
+                  <path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z" />
+                </svg>
+                <a href="mailto:wiktor.franc@wp.pl?subject=Kontakt z jeszczeciepłe">
+                  email
+                </a>
+              </div>
+
+              <div className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 -960 960 960"
+                  className="min-w-6 w-6"
+                >
+                  <path d="M798-120q-125 0-247-54.5T329-329Q229-429 174.5-551T120-798q0-18 12-30t30-12h162q14 0 25 9.5t13 22.5l26 140q2 16-1 27t-11 19l-97 98q20 37 47.5 71.5T387-386q31 31 65 57.5t72 48.5l94-94q9-9 23.5-13.5T670-390l138 28q14 4 23 14.5t9 23.5v162q0 18-12 30t-30 12ZM241-600l66-66-17-94h-89q5 41 14 81t26 79Zm358 358q39 17 79.5 27t81.5 13v-88l-94-19-67 67ZM241-600Zm358 358Z" />
+                </svg>
+              </div>
+
+              <div className="flex gap-2 items-start">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 -960 960 960"
+                  className="min-w-6 w-6"
+                >
+                  <path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 294q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z" />
+                </svg>
+
+                <div className="flex flex-col">
+                  <div>AAA 22</div>
+                  <div>BBB 22</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex-2">
+              <h2 className="text-2xl font-bold">Lokalizacja</h2>
+            </div>
+          </div>
+
+          <div className="py-4 text-center text-sm border-t border-t-gray-400">
+            2025 - kanapki.pl
+          </div>
+        </div>
+      </footer>
+    </>
   );
 }
