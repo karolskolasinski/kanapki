1d5b97d37ad92d73c95c9376e2da3398f6a950bc
Karol Skolasiński <karol.skolasinski@7willows.com>
2025-05-30 15:39:46 +0200

    feat: style menu

--- components/MenuItem.tsx
+++ components/MenuItem.tsx
@@ -13,6 +13,20 @@ type MenuItemProps = {
   userId?: string;
 };
 
+function formatDishLabels(dish: Dish): string {
+  return [
+    dish.vegetarian && "wege",
+    dish.vegan && "vegan",
+    dish.glutenFree && "bez glutenu",
+    dish.lactoseFree && "bez laktozy",
+    dish.sugarFree && "bez cukru",
+    dish.kcal && `${dish.kcal}kcal`,
+    dish.protein && `Białko ${dish.protein}g`,
+    dish.fat && `Tłuszcze: ${dish.fat}g`,
+    dish.carbs && `Węglowodany ${dish.carbs}g`,
+  ].filter(Boolean).join(", ");
+}
+
 export default function MenuItem(props: MenuItemProps) {
   const { dish, checked, showLabel, userId } = props;
   const label = dish.category === "jc" ? "Jeszcze ciepłe" : "Nieźle zmrożone";
@@ -26,11 +40,18 @@ export default function MenuItem(props: MenuItemProps) {
         {userId && <Switcher checked={checked} dishId={dish.id!} userId={userId} />}
 
         <div className="flex-1 flex items-baseline">
-          <div className="">{dish.name}</div>
+          {!userId && (dish.vegetarian || dish.vegan) && (
+            <div className="self-center">
+              <Image src="/vege.svg" alt="Wege lub vegan" width={23} height={23} />
+            </div>
+          )}
+          <div>{dish.name}</div>
+
+          {dish.weight && <small className="text-xs px-1">{dish.weight}g</small>}
 
           <div className="flex-1 w-22 h-[2px] bg-repeat-x bg-center bg-[length:8px_2px] bg-[url('/dot.svg')]" />
 
-          <div className="">{dish.price}</div>
+          <div>{dish.price}</div>
         </div>
 
         {userId && (
@@ -40,12 +61,20 @@ export default function MenuItem(props: MenuItemProps) {
         )}
       </div>
 
-      <div className={`${fontWeightClass} text-xs pt-3`}>
-        {ingredients
-          .filter((i) => dish.ingredients?.includes(i.id!))
-          .map((i) => i.name)
-          .join(", ")}
-      </div>
+      {!userId && (
+        <div className={`${fontWeightClass} text-xs pt-3`}>
+          {formatDishLabels(dish)}
+        </div>
+      )}
+
+      {!userId && (
+        <div className={`${fontWeightClass} text-xs`}>
+          skład: {ingredients
+            .filter((i) => dish.ingredients?.includes(i.id!))
+            .map((i) => i.name)
+            .join(", ")}
+        </div>
+      )}
     </div>
   );
 }
