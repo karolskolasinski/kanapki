0a02999f1d9e568b91083fdb77c8d5b141236364
Karol Skolasiński <karol.skolasinski@7willows.com>
2025-05-30 20:06:34 +0200

    feat: style dots

--- components/MenuItem.tsx
+++ components/MenuItem.tsx
@@ -36,40 +36,53 @@ export default function MenuItem(props: MenuItemProps) {
   return (
     <div className="pb-5">
       {showLabel && <h2 className="my-3 font-semibold">{label}</h2>}
-      <div className="flex">
+
+      <div className="flex items-baseline">
         {userId && <Switcher checked={checked} dishId={dish.id!} userId={userId} />}
 
-        <div className="flex-1 flex items-baseline">
+        <div>
           {!userId && (dish.vegetarian || dish.vegan) && (
-            <div className="self-center">
-              <Image src="/vege.svg" alt="Wege lub vegan" width={23} height={23} />
-            </div>
+            <Image
+              src="/vege.svg"
+              alt="Wege lub vegan"
+              width={24}
+              height={24}
+              className="inline"
+            />
           )}
-          <div>{dish.name}</div>
 
-          {dish.weight && <small className="text-xs px-1">{dish.weight}g</small>}
+          <span className="text-base align-middle">{dish.name}</span>
 
-          <div className="flex-1 w-22 h-[2px] bg-repeat-x bg-center bg-[length:8px_2px] bg-[url('/dot.svg')]" />
+          {dish.weight && <small className="text-xs pl-1 align-text-bottom">{dish.weight}g</small>}
+        </div>
 
-          <div>{dish.price}</div>
+        <div className="self-end grow flex px-1">
+          <div className="min-w-4 grow self-baseline h-[2px] bg-repeat-x bg-center bg-[length:8px_2px] bg-[url('/dot.svg')]" />
+          <span className="self-baseline">{dish.price}</span>
         </div>
 
         {userId && (
           <Link href={`/dashboard/dishes/${dish.id}`} className="pl-3">
-            <Image src="/edit.svg" alt="edycja" width={20} height={20} />
+            <Image
+              src="/edit.svg"
+              alt="edycja"
+              width={20}
+              height={20}
+              className="inline"
+            />
           </Link>
         )}
       </div>
 
       {!userId && (
-        <div className={`${fontWeightClass} text-xs pt-3`}>
+        <div className={`${fontWeightClass} text-[.7rem] xs:text-xs pt-3 pb-2`}>
           {formatDishLabels(dish)}
         </div>
       )}
 
       {!userId && (
-        <div className={`${fontWeightClass} text-xs`}>
-          skład: {ingredients
+        <div className={`${fontWeightClass} text-[.7rem] xs:text-xs`}>
+          <span className="font-bold">skład:</span> {ingredients
             .filter((i) => dish.ingredients?.includes(i.id!))
             .map((i) => i.name)
             .join(", ")}
